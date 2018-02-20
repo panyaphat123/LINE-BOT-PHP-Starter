6e5676cfc4e923691b27c9364f66803348ea86d5
panyaphat123 <panyapattaithey@gmail.com>
2018-02-20 14:26:09 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = '
-4Orkno14VxP2MCgrmYr/2i3oSVbhsrSezaLCKm59xrcc64fHAG75lbQWN8A9nzQJJWVakIyjXuqEsAoW7FLdvBKIXdA0PWU967QL30+LHsjESkz1N+bj9z9vtJUFdc7QxHGD5n/UFap6tNCdNOFxegdB04t89/1O/w1cDnyilFU=';
+4Orkno14VxP2MCgrmYr/2i3oSVbhsrSezaLCKm59xrcc64fHAG75lbQWN8A9nzQJJWVakIyjXuqEsAoW7FLdvBKIXdA0PWU967QL30+LHsjESkz1N+bj9z9vtJUFdc7QxHGD5n/UFap6tNCdNOFxegdB04t89/1O/w1cDnyilFU
+=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -14,3 +15,4 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+?>
