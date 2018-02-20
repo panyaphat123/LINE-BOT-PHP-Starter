371f84be200e88a8114ec3cfd038fe5172ce82aa
panyaphat123 <panyapattaithey@gmail.com>
2018-02-20 15:05:50 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,11 +1,5 @@
 <?php
-$access_token = '
-Q7Vn7Mx5VFZ3bWoMuQiaEA+D3HfLA/PK3SDI+n+sNsX38J
-
-+wtXUwiTfG48Savx1dJWVakIyjXuqEsAoW7FLdvBKIXdA0PWU967QL30+LHshk9RjRSX1v
-
-Bidr2hHgXn7jkXBzfwkF0iEc7D9UCUg7qQdB04t89/1O/w1cDnyilFU= ';
-
+$access_token = 'Q7Vn7Mx5VFZ3bWoMuQiaEA+D3HfLA/PK3SDI+n+sNsX38J+wtXUwiTfG48Savx1dJWVakIyjXuqEsAoW7FLdvBKIXdA0PWU967QL30+LHshk9RjRSX1vBidr2hHgXn7jkXBzfwkF0iEc7D9UCUg7qQdB04t89/1O/w1cDnyilFU= ';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
